23b4d5bae971d21f774e6d342bb3cb911eaab84e
Peter van Diepen <pvdiepen@gmail.com>
2021-03-15 02:26:07 +0100

    Tweede Kamerverkiezingen vanaf 2002

--- public/js/tk.js
+++ public/js/tk.js
@@ -1,6 +1,38 @@
 "use strict";
 
 const tk = [
+    {jaar: 2002,
+        zetels: "CDA=43&LPF=26&VVD=24&PvdA=23&GL=10&SP=9&D66=7&CU=4&SGP=2&LN=2",
+        kabinet: "Balkenende1",
+        coalitie: "CDA, LPF, VVD",
+        breed: 1280,
+        hoog: 720,
+        link: "https://nl.wikipedia.org/wiki/Kabinet-Balkenende_I"
+    },
+    {jaar: 2003,
+        zetels: "CDA=44&PvdA=42&VVD=28&SP=9&LPF=8&GL=8&D66=6&CU=3&SGP=2",
+        kabinet: "Balkenende2",
+        coalitie: "CDA, VVD, D66",
+        breed: 1280,
+        hoog: 754,
+        link: "https://nl.wikipedia.org/wiki/Kabinet-Balkenende_II"
+    },
+    {jaar: 2006,
+        zetels: "CDA=41&PvdA=33&SP=25&VVD=22&PVV=9&GL=7&CU=6&D66=3&PvdD=2&SGP=2",
+        kabinet: "Balkenende4",
+        coalitie: "CDA, PvdA, CU",
+        breed: 960,
+        hoog: 629,
+        link: "https://nl.wikipedia.org/wiki/Kabinet-Balkenende_IV"
+    },
+    {jaar: 2010,
+        zetels: "VVD=31&PvdA=30&PVV=24&CDA=21&SP=15&D66=10&GL=10&CU=5&SGP=2&PvdD=2",
+        kabinet: "Rutte1",
+        coalitie: "VVD, CDA",
+        breed: 1280,
+        hoog: 658,
+        link: "https://nl.wikipedia.org/wiki/Kabinet-Rutte_I"
+    },
     {jaar: 2012,
         zetels: "VVD=41&PvdA=38&PVV=15&SP=15&CDA=13&D66=12&CU=5&GL=4&SGP=3&PvdD=2&50plus=2",
         kabinet: "Rutte2",
@@ -19,7 +51,7 @@ const tk = [
     },
     {jaar: 2021,
         zetels: "VVD=39&PVV=19&CDA=17&D66=15&GL=11&SP=10&PvdA=13&CU=6&PvdD=6&50plus=3&SGP=3&Denk=2&FvD=4&Bij1=0&JA21=1&CodeOranje=0&Volt=1",
-        kabinet: "Peiling",
+        kabinet: "Onbekend",
         coalitie: "peilingwijzer",
         breed: 600,
         hoog: 430,
