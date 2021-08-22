4f51e4aac67d92bbd4105af2ca8368978d20d183
Peter van Diepen <pvdiepen@gmail.com>
2021-08-22 10:22:40 +0200

    indelenEersteRonde

--- public/js/indelen.js
+++ public/js/indelen.js
@@ -36,6 +36,12 @@ async function ranglijstDeelnemers(datumTot) {
 }
 
 function deelnemers(lijst, ranglijst) {
+    ranglijst.forEach(function(t, i) {
+        lijst.appendChild(htmlRij(i + 1, t.naam, t.punten(), t.rating()));
+    });
+}
+
+function deelnemers0(lijst, ranglijst) {
     for (let i = 0; i < ranglijst.length; i++) {
         const t = ranglijst[i];
         lijst.appendChild(htmlRij(i + 1, t.naam, t.punten(), t.rating()));
@@ -43,10 +49,53 @@ function deelnemers(lijst, ranglijst) {
 }
 
 function partijen(lijst, ranglijst) {
-    const helft = ranglijst.length / 2;
-    for (let i = 0; i < helft; i++) {
-        const s = ranglijst[i];
-        const t = ranglijst[i + helft];
-        lijst.appendChild(htmlRij(i + 1, s.naam, t.naam, `${i+1} - ${i+helft+1}`));
+    let wit = [];
+    let zwart = [];
+    indelenEersteRonde(evenAantal(ranglijst.length), 3, wit, zwart);
+    for (let i = 0; i < wit.length; i++) {
+        lijst.appendChild(htmlRij(i + 1, ranglijst[wit[i]].naam, ranglijst[zwart[i]].naam, `${wit[i]+1} - ${zwart[i]+1}`));
+    }
+}
+
+function evenAantal(aantal) {
+    return aantal % 2 === 0 ? aantal : aantal + 1;
+}
+
+function indelenEersteRonde(aantalSpelers, aantalGroepen, wit, zwart) {
+    const aantalPartijen = aantalSpelers / 2;
+    aantalGroepen = juisteAantalGroepen(aantalGroepen, aantalSpelers);
+    const helftGroep = Math.ceil(aantalPartijen / aantalGroepen);
+    const tot = (aantalGroepen - 1) * helftGroep;
+    for (let van = 0; van < tot; van += helftGroep) {  // laatste groep niet indelen
+        groepIndelenEersteRonde(van, van + helftGroep, wit, zwart);
+    }
+    if (tot < aantalPartijen) {
+        groepIndelenEersteRonde(tot, aantalPartijen, wit, zwart); // laatste groep
+    }
+}
+
+function juisteAantalGroepen(aantalGroepen, aantalSpelers) {
+    if (aantalSpelers < 7) {
+        return 1;
+    } else if (aantalSpelers < 11 && aantalGroepen > 2) {
+        return 2;
+    } else if (aantalSpelers < 15 && aantalGroepen > 3) {
+        return 3;
+    } else if (aantalGroepen > 4) { // TODO testen met meer dan 4 groepen
+        return 4;
+    } else {
+        return aantalGroepen;
+    }
+}
+
+function groepIndelenEersteRonde(van, tot, wit, zwart) {
+    for (let i = van; i < tot; i++) {
+        if (i % 2 == 0) {
+            wit[i] = i + tot;
+            zwart[i] = i + van;
+        } else {
+            wit[i] = i + van;
+            zwart[i] = i + tot;
+        }
     }
 }
