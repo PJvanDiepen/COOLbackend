aaa08ddf5ea93edb5c821bc04bba332d2e802716
Peter van Diepen <pvdiepen@gmail.com>
2021-09-20 13:13:35 +0200

    0-0-0.nl versie 0.5.9 metWit en overslaan afdrukken indien indelen niet lukt

--- public/js/beheer.js
+++ public/js/beheer.js
@@ -18,7 +18,7 @@ TODO mutaties met verwijderen
     gebruikers(document.getElementById("gebruikers"));
     laatsteMutaties(document.getElementById("mutaties"));
     document.getElementById("computer").appendChild(
-        htmlTekst(`0-0-0.nl versie 0.5.8 met operating system: ${navigator.platform} en browser: ${navigator.vendor}`));  // TODO client hints
+        htmlTekst(`0-0-0.nl versie 0.5.9 met operating system: ${navigator.platform} en browser: ${navigator.vendor}`));  // TODO client hints
 })();      // TODO 0-0-0 versie uit package.json
 
 async function backup(tabel) {
