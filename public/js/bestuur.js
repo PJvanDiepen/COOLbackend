3ec7322283c397530b1580c6685ec03bd8741388
Peter van Diepen <pvdiepen@gmail.com>
2022-08-19 10:27:05 +0200

    csv met regex verwerkt regel tot kolommen

--- public/js/bestuur.js
+++ public/js/bestuur.js
@@ -76,58 +76,86 @@ function theFileReader() {
             output.innerHTML = "Could not read file, error code is " + reader.error.code;
         };
         reader.onload = function() {
-            const regex = /(),|(\d*),|"([^"]*)",|([^,]*),/g;
             const regels = reader.result.split('\n');
             for (const regel of regels) {
-                regelVerwerken(regex, regel);
+                olaVerwerken(csv(regel));
             }
-            output.innerHTML = `${files[0].name} ${files[0].type} ${files[0].size} bytes gelezen`;
-            // console.log(regel);
+            output.innerHTML = `${files[0].name} type: ${files[0].type} size: ${files[0].size}`;
         };
     });
 }
 
-/*
-01 Relatienummer > knsbNummer
-02 Volledige naam > voornaam, tussenvoegsel, achternaam
-03 Geslacht > M of V
-04 Geboortejaar
-05 Categorie > S of J
-06 Email
-07 Telefoonnummer 1
-08 Telefoonnummer 2
-09 Adres > straat, huisNummer
-10 Postcode
-11 Plaatsnaam
-12 Landnaam
-13 Lidmaatschap
-14 Lid sinds
-15 Afgemeld per
-16 KNSB-rating > knsbRating
-17 FIDE-rating
-18 Jeugdrating
-19 Stappenniveau
-20 Verlengingsdatum
-21 Gebruik NAW
-22 Beeldmateriaal
- */
+const regex = /(),|(\d*),|"([^"]*)",|([^,]*),/g;
 
-function regelVerwerken(regex, regel) {
-    console.log("--- regelVerwerken ---");
-    console.log(regel);
+/**
+ * Zie https://regex101.com/codegen?language=javascript
+ *
+ * bovenstaande regular expression onderscheid kolommen geschieden door komma's
+ * per kolom is er een groep met een komma
+ * groep 0 is de complete match
+ * groep 1 indien leeg
+ * groep 2 indien getal
+ * groep 3 indien tekst met "" (eventueel met komma's)
+ * groep 4 indien tekst zonder ""
+ *
+ * csv verwerkt een regel tot kolommen met behulp van regex
+ * behalve de laatste kolom, want die heeft geen komma
+ *
+ * @param regel in csv
+ * @returns {*[]} kolommen
+ */
+function csv(regel) {
+    let kolommen = [];
     let m;
     while ((m = regex.exec(regel)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
         if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
+            regex.lastIndex++; // This is necessary to avoid infinite loops with zero-width matches
+        } else {
+            m.forEach(function (match, groupIndex) {
+                if (groupIndex === 1 && match !== undefined) {
+                    kolommen.push("");
+                } else if (groupIndex === 2 && match !== undefined) {
+                    kolommen.push(Number(match));
+                } else if (groupIndex === 3 && match !== undefined) {
+                    kolommen.push(match);
+                } else if (groupIndex === 4 && match !== undefined) {
+                    kolommen.push(match);
+                }
+            });
         }
-
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-            console.log(`Found match, group ${groupIndex}: ${match}`);
-        });
     }
+    return kolommen;
 }
+
+/*
+00 Relatienummer > knsbNummer
+01 Volledige naam > voornaam, tussenvoegsel, achternaam
+02 Geslacht > M of V
+03 Geboortejaar
+04 Categorie > S of J
+05 Email
+06 Telefoonnummer 1
+07 Telefoonnummer 2
+08 Adres > straat, huisNummer
+09 Postcode
+10 Plaatsnaam
+11 Landnaam
+12 Lidmaatschap
+13 Lid sinds
+14 Afgemeld per
+15 KNSB-rating > knsbRating
+16 FIDE-rating
+17 Jeugdrating
+18 Stappenniveau
+19 Verlengingsdatum
+20 Gebruik NAW
+21 Beeldmateriaal (laatste kolom ontbreekt!)
+ */
+
+function olaVerwerken(kolom) {
+    console.log(`${kolom[1]} ${kolom[0]} ${kolom[5]} speelt ${kolom[4] === "S" ? "dinsdag" : "niet"} ${kolom[15]}`);
+}
+
 
 function drieGroepen() {
     const regex = /"(\d*),""([^"]*)"",([^"]*)";/gm;
