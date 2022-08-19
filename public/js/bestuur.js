c6e5d180ea405cd05b868bd2d460f5369110f927
Peter van Diepen <pvdiepen@gmail.com>
2022-08-19 18:07:59 +0200

    normaleNaam van olaNaam met regex

--- public/js/bestuur.js
+++ public/js/bestuur.js
@@ -6,10 +6,7 @@
             naarAnderePagina("beheer.html");
         }]);
     ledenLijst(document.getElementById("kop"), document.getElementById("tabel"));
-    // testRegEx1();
-    theFileReader();
-    // drieGroepen();
-    // meerGroepen();
+    olaBestandLezen();
 })();
 
 async function ledenLijst(kop, lijst) {
@@ -34,38 +31,9 @@ async function ledenLijst(kop, lijst) {
 }
 
 /*
-Zie https://regex101.com/codegen?language=javascript
- */
-function testRegEx1() {
-    const regex = /(.*),.*\.\s(.*)\((.*)\)/gm;
-
-// Alternative syntax using RegExp constructor
-// const regex = new RegExp('(.*),.*\\.\\s(.*)\\((.*)\\)', 'gm')
-
-    const str = `Diepen, P.J. van (Peter)
-Bakker, J. (Jos)
-Meiden, D. van der (Dirk)
-Horst, C.A. v.d. (Corné)
-`;
-    let m;
-
-    while ((m = regex.exec(str)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
-        }
-
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-            console.log(`Found match, group ${groupIndex}: ${match}`);
-        });
-    }
-}
-
-/*
 Zie Matt Frisbie: Professional JavaScript for Web Developers blz. 760
  */
-function theFileReader() {
+function olaBestandLezen() {
     const filesList = document.getElementById("olaFile");
     filesList.addEventListener("change", function (event) {
         const output = document.getElementById("output");
@@ -80,16 +48,14 @@ function theFileReader() {
             for (const regel of regels) {
                 olaVerwerken(csv(regel));
             }
-            output.innerHTML = `${files[0].name} type: ${files[0].type} size: ${files[0].size}`;
+            output.innerHTML = `${files[0].size} bytes van ${files[0].name} zijn ingelezen`;
         };
     });
 }
 
-const regex = /(),|(\d*),|"([^"]*)",|([^,]*),/g;
+const olaRegex = /(),|(\d*),|"([^"]*)",|([^,]*),/g;
 
 /**
- * Zie https://regex101.com/codegen?language=javascript
- *
  * bovenstaande regular expression onderscheid kolommen geschieden door komma's
  * per kolom is er een groep met een komma
  * groep 0 is de complete match
@@ -98,7 +64,9 @@ const regex = /(),|(\d*),|"([^"]*)",|([^,]*),/g;
  * groep 3 indien tekst met "" (eventueel met komma's)
  * groep 4 indien tekst zonder ""
  *
- * csv verwerkt een regel tot kolommen met behulp van regex
+ * Zie https://regex101.com/codegen?language=javascript
+ *
+ * csv verwerkt een regel tot kolommen met behulp van olaRegex
  * behalve de laatste kolom, want die heeft geen komma
  *
  * @param regel in csv
@@ -107,9 +75,9 @@ const regex = /(),|(\d*),|"([^"]*)",|([^,]*),/g;
 function csv(regel) {
     let kolommen = [];
     let m;
-    while ((m = regex.exec(regel)) !== null) {
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++; // This is necessary to avoid infinite loops with zero-width matches
+    while ((m = olaRegex.exec(regel)) !== null) {
+        if (m.index === olaRegex.lastIndex) {
+            olaRegex.lastIndex++; // This is necessary to avoid infinite loops with zero-width matches
         } else {
             m.forEach(function (match, groupIndex) {
                 if (groupIndex === 1 && match !== undefined) {
@@ -125,6 +93,40 @@ function csv(regel) {
         }
     }
     return kolommen;
+}
+
+const naamRegex = /(.*),.*\.\s(.*)\((.*)\)/;
+
+/**
+ * maak van de olaNaam een normaleNaam
+ *
+ * @param olaNaam uit Online Leden Administratie van KNSB
+ * @returns {string|*} voorNaam tussenvoegsel achterNaam
+ *
+ * Diepen, P.J. van (Peter) -> Peter van Diepen
+ * Bakker, J. (Jos) -> Jos Bakker
+ * Meiden, D. van der (Dirk) -> Dirk van der Meiden
+ * Horst, C.A. v.d. (Corné) -> Corné Horst (gaat fout wegens tussenvoegsel met punten!)
+ *
+ */
+function normaleNaam(olaNaam) {
+    let voornaam = "";
+    let tussenvoegsel = "";
+    let achternaam = "";
+    let m;
+    if ((m = naamRegex.exec(olaNaam)) !== null) {
+        m.forEach(function (match, groupIndex) {
+            if (groupIndex === 3) {
+                voornaam = match;
+            } else if (groupIndex === 2) {
+                tussenvoegsel = match;
+            } else if (groupIndex === 1) {
+                achternaam = match;
+            }
+        });
+        return `${voornaam} ${tussenvoegsel}${achternaam}`; // geen spatie tussen tussenvoegsel en achternaam!
+    }
+    return olaNaam;
 }
 
 /*
@@ -153,54 +155,7 @@ function csv(regel) {
  */
 
 function olaVerwerken(kolom) {
-    console.log(`${kolom[1]} ${kolom[0]} ${kolom[5]} speelt ${kolom[4] === "S" ? "dinsdag" : "niet"} ${kolom[15]}`);
-}
-
-
-function drieGroepen() {
-    const regex = /"(\d*),""([^"]*)"",([^"]*)";/gm;
-
-// Alternative syntax using RegExp constructor
-// const regex = new RegExp('"(\\d*),""([^"]*)"",([^"]*)";', 'gm')
-
-    const str = `"7970094,""Ruiter, D.E. de (Danny)"",M,1991,S,dannyderuiter@hotmail.com,072-5159175,06-53269132,De Kempenstraat 7,1827 AG,Alkmaar,Nederland,Dubbellid,1/7/2021,,2271,2251,,,,Toegestaan,Toegestaan";;;;;;;;;;;;;;;;;;;;;;;;;"7970094,""Ruiter, D.E. de (Danny)"",M,1991,S,dannyderuiter@hotmail.com,072-5159175,06-53269132,De Kempenstraat 7,1827 AG,Alkmaar,Nederland,Dubbellid,1/7/2021,,2271,2251,,,,Toegestaan,Toegestaan%%%%%%%%%%%%%%%%%%%%%%"
-`;
-    let m;
-
-    while ((m = regex.exec(str)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
-        }
-        console.log("--- dit is m:");
-        console.log(m);
-        console.log("--- tot hier");
-
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-            console.log(`Found match, group ${groupIndex}: ${match}`);
-        });
-    }
-}
-
-function meerGroepen () {
-    const regex = /([^,]*)/gm;
-
-// Alternative syntax using RegExp constructor
-// const regex = new RegExp('([^,]*)', 'gm')
-
-    const str = `M,1991,S,dannyderuiter@hotmail.com,072-5159175,06-53269132,De Kempenstraat 7,1827 AG,Alkmaar,Nederland,Dubbellid,1/7/2021,,2271,2251,,,,Toegestaan,Toegestaan`;
-    let m;
-
-    while ((m = regex.exec(str)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
-        }
-
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-            console.log(`Found match, group ${groupIndex}: ${match}`);
-        });
+    if (kolom[0] !== undefined) {
+        console.log(`${normaleNaam(kolom[1])} ${kolom[0]} ${kolom[5]} speelt ${kolom[4] === "S" ? "dinsdag" : "niet"} ${kolom[15]}`);
     }
 }
