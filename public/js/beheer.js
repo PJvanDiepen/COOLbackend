28ae474f7979f5d20affe74efa8d7fb48267fd63
Peter van Diepen <pvdiepen@gmail.com>
2023-05-08 20:59:26 +0200

    0-0-0.nl versie 0.8.22

--- public/js/beheer.js
+++ public/js/beheer.js
@@ -1,8 +1,8 @@
 "use strict";
 
-import { BEHEERDER, hoera } from "/modules/db.js";
+// import { BEHEERDER, hoera } from "./modules/db.js";
 
-import * as zyq from "/modules/zyq.js";
+// import * as zyq from "./modules/zyq.js";
 
 /*
 TODO mutaties per gebruiker afsplitsen
@@ -11,63 +11,63 @@ TODO mutaties met verwijderen
  */
 
 (async function() {
-    await zyq.init();
-    zyq.menu([BEHEERDER, `backup gebruikers` , async function () {
-            const rijen = await zyq.serverFetch(`/${zyq.uuidToken}/backup/gebruiker`);
-            zyq.backupSQL("gebruiker", rijen);
+    await init();
+    menu([BEHEERDER, `backup gebruikers` , async function () {
+            const rijen = await serverFetch(`/${uuidToken}/backup/gebruiker`);
+            backupSQL("gebruiker", rijen);
         }],
         [BEHEERDER, `backup personen` , async function () {
-            const rijen = await zyq.serverFetch(`/backup/persoon`);
-            zyq.backupSQL("persoon", rijen);
+            const rijen = await serverFetch(`/backup/persoon`);
+            backupSQL("persoon", rijen);
         }],
-        [BEHEERDER, `backup spelers ${zyq.seizoenVoluit(zyq.o_o_o.seizoen)}` , async function () {
-            const rijen = await zyq.serverFetch(`/backup/speler/${zyq.o_o_o.seizoen}`);
-            zyq.backupSQL("speler", rijen);
+        [BEHEERDER, `backup spelers ${seizoenVoluit(o_o_o.seizoen)}` , async function () {
+            const rijen = await serverFetch(`/backup/speler/${o_o_o.seizoen}`);
+            backupSQL("speler", rijen);
         }],
-        [BEHEERDER, `backup teams ${zyq.seizoenVoluit(zyq.o_o_o.seizoen)}` , async function () {
-            const rijen = await zyq.serverFetch(`/backup/team/${zyq.o_o_o.seizoen}`);
-            zyq.backupSQL("team", rijen);
+        [BEHEERDER, `backup teams ${seizoenVoluit(o_o_o.seizoen)}` , async function () {
+            const rijen = await serverFetch(`/backup/team/${o_o_o.seizoen}`);
+            backupSQL("team", rijen);
         }],
-        [BEHEERDER, `backup ronden ${zyq.seizoenVoluit(zyq.o_o_o.seizoen)}` , async function () {
-            const rijen = await zyq.serverFetch(`/backup/ronde/${zyq.o_o_o.seizoen}`);
-            zyq.backupSQL("ronde", rijen);
+        [BEHEERDER, `backup ronden ${seizoenVoluit(o_o_o.seizoen)}` , async function () {
+            const rijen = await serverFetch(`/backup/ronde/${o_o_o.seizoen}`);
+            backupSQL("ronde", rijen);
         }],
         [BEHEERDER, "test API", function () {
-            zyq.naarAnderePagina("api.html");
+            naarAnderePagina("api.html");
         }]);
     gebruikers(document.getElementById("gebruikers"));
     laatsteMutaties(document.getElementById("mutaties"));
-    const versie = await zyq.serverFetch(`/versie`);
-    console.log(hoera());
+    const versie = await serverFetch(`/versie`);
+    // console.log(hoera());
     document.getElementById("computer").appendChild(
-        zyq.htmlTekst(`0-0-0 versie ${versie} met browser: ${navigator.vendor}`));  // TODO client hints
+        htmlTekst(`0-0-0 versie ${versie} met browser: ${navigator.vendor}`));  // TODO client hints
 })();
 
 async function gebruikers(lijst) {
-    const leden = await zyq.serverFetch(`/${zyq.uuidToken}/gebruikers`);
+    const leden = await serverFetch(`/${uuidToken}/gebruikers`);
     let aantal = 0;
     for (const lid of leden) {
-        lijst.appendChild(zyq.htmlRij(
+        lijst.appendChild(htmlRij(
             ++aantal,
-            zyq.naarSpeler(lid),
-            zyq.gebruiker.mutatieRechten === BEHEERDER ? gebruikerEmailSturen(lid) : lid.email,
-            zyq.gebruikerFunctie(lid)));
+            naarSpeler(lid),
+            gebruiker.mutatieRechten === BEHEERDER ? gebruikerEmailSturen(lid) : lid.email,
+            gebruikerFunctie(lid)));
     }
 }
 
 function gebruikerEmailSturen(lid) {
-    return zyq.htmlLink(`email.html?speler=${lid.knsbNummer}&email=${lid.email}`, lid.email);
+    return htmlLink(`email.html?speler=${lid.knsbNummer}&email=${lid.email}`, lid.email);
 }
 
 // TODO gebruiker hoger of lagere functie geven
 
 async function laatsteMutaties(lijst) {
-    const mutaties = await zyq.serverFetch(`/${zyq.uuidToken}/mutaties/0/9/100`); // laatste 100 mutaties
+    const mutaties = await serverFetch(`/${uuidToken}/mutaties/0/9/100`); // laatste 100 mutaties
     let vorige = 0;
     for (const mutatie of mutaties) {
-        lijst.appendChild(zyq.htmlRij(
-            zyq.tijdGeleden(mutatie.tijdstip),
-            mutatie.knsbNummer === vorige ? "" : zyq.naarSpeler(mutatie),
+        lijst.appendChild(htmlRij(
+            tijdGeleden(mutatie.tijdstip),
+            mutatie.knsbNummer === vorige ? "" : naarSpeler(mutatie),
             mutatie.knsbNummer === vorige ? "" : mutatie.knsbNummer,
             mutatie.url,
             mutatie.aantal,
