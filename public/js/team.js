2bdf072e36b7fafbe678187c193a9d4d2f429d77
Peter van Diepen <pvdiepen@gmail.com>
2023-05-14 23:48:46 +0200

    team.js met zyq.js i.p.v. const.js

--- public/js/team.js
+++ public/js/team.js
@@ -1,27 +1,31 @@
 "use strict";
 
+import * as db from "./db.js";
+
+import * as zyq from "./zyq.js";
+
 /*
 verwerk team=<teamCode>
  */
 (async function() {
-    await init();
-    menu([BEHEERDER, "backup uitslagen van alle ronden" , async function () {
-            const rijen = await serverFetch(`/backup/ronde/uitslag/${o_o_o.seizoen}/${o_o_o.team}/1/9`);
-            backupSQL("uitslag", rijen);
+    await zyq.init();
+    zyq.menu([db.BEHEERDER, "backup uitslagen van alle ronden" , async function () {
+            const rijen = await zyq.serverFetch(`/backup/ronde/uitslag/${zyq.o_o_o.seizoen}/${zyq.o_o_o.team}/1/9`);
+            zyq.backupSQL("uitslag", rijen);
         }]);
-    await teamSelecteren(o_o_o.team);
+    await zyq.teamSelecteren(zyq.o_o_o.team);
     await uitslagenTeam(document.getElementById("kop"), document.getElementById("ronden"));
 })();
 
 async function uitslagenTeam(kop, rondenTabel) {
-    const teams = await localFetch("/teams/" + o_o_o.seizoen);
+    const teams = await zyq.localFetch(`/teams/${zyq.o_o_o.seizoen}`);
     for (const team of teams) {
-        if (team.teamCode === o_o_o.team) {
-            kop.innerHTML = [teamVoluit(o_o_o.team), seizoenVoluit(o_o_o.seizoen), team.omschrijving].join(SCHEIDING);
+        if (team.teamCode === zyq.o_o_o.team) {
+            kop.innerHTML = [zyq.teamVoluit(zyq.o_o_o.team), zyq.seizoenVoluit(zyq.o_o_o.seizoen), team.omschrijving].join(zyq.SCHEIDING);
             break;
         }
     }
-    const rondeUitslagen = await uitslagenTeamAlleRonden(o_o_o.team);
+    const rondeUitslagen = await zyq.uitslagenTeamAlleRonden(zyq.o_o_o.team);
     for (let i = 0; i < rondeUitslagen.length; ++i) {
         uitslagenTeamPerRonde(rondeUitslagen[i], i + 1, rondenTabel);
     }
@@ -29,14 +33,14 @@ async function uitslagenTeam(kop, rondenTabel) {
 
 function uitslagenTeamPerRonde(u, rondeNummer, rondenTabel) {
     if (u) { // eventueel ronde overslaan, wegens oneven aantal teams in een poule
-        const datumKolom = datumLeesbaar(u.ronde);
-        const uitslagKolom = uitslagTeam(u.ronde.uithuis, u.winst, u.verlies, u.remise);
-        rondenTabel.appendChild(htmlRij(u.ronde.rondeNummer, datumKolom, naarTeam(u.ronde), uitslagKolom));
+        const datumKolom = zyq.datumLeesbaar(u.ronde);
+        const uitslagKolom = zyq.uitslagTeam(u.ronde.uithuis, u.winst, u.verlies, u.remise);
+        rondenTabel.appendChild(zyq.htmlRij(u.ronde.rondeNummer, datumKolom, zyq.naarTeam(u.ronde), uitslagKolom));
         if (u.uitslagen.length) {
             const div = document.getElementById("ronde" + rondeNummer); // 9 x div met id="ronde1".."ronde9"
-            div.appendChild(document.createElement("h2")).innerHTML = ["Ronde " + rondeNummer, datumKolom].join(SCHEIDING);
+            div.appendChild(document.createElement("h2")).innerHTML = ["Ronde " + rondeNummer, datumKolom].join(zyq.SCHEIDING);
             const tabel = div.appendChild(document.createElement("table"));
-            tabel.appendChild(htmlRij("", wedstrijdVoluit(u.ronde), "", uitslagKolom));
+            tabel.appendChild(zyq.htmlRij("", zyq.wedstrijdVoluit(u.ronde), "", uitslagKolom));
             for (let uitslag of u.uitslagen) {
                 tabel.appendChild(uitslag);
             }
